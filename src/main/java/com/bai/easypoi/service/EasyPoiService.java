40f289df5cd24429fa94b07713bb972c9753761a
a-chenhonglei <12369874ff>
2019-01-25 11:49:05 +0800

    添加运行结果说明

--- src/main/java/com/bai/easypoi/service/EasyPoiService.java
+++ src/main/java/com/bai/easypoi/service/EasyPoiService.java
@@ -26,7 +26,7 @@ public class EasyPoiService {
         //验证 备注：3.0的poi版本开启验证的设置setNeedVerfiy 名称错误
        params.setNeedVerify(true);
         try {
-
+            //NO.1 第一种方式map的格式
             long start = System.currentTimeMillis();
             List<Map> entities1 = ExcelImportUtil.importExcel(
                     uploadFile.getInputStream(),
@@ -38,14 +38,21 @@ public class EasyPoiService {
             for (Map map: entities1){
                 System.out.println(map);
             }
+            /**
+             * NO.2 第二种格式数据对象的格式
+             * 需要注意的是 数据对象里的实体类型要与 excel模板中的数据类型对应
+             */
+
             ImportParams p = new ImportParams();
             p.setHeadRows(1);
             p.setTitleRows(1);
+            //开启验证
             p.setNeedVerify(true);
 
             ExcelImportResult<LineExcelEntity> result = ExcelImportUtil.importExcelMore(uploadFile.getInputStream(),
                                                         LineExcelEntity.class,
                                                         p);
+            //获取读取成功的数据
             List<LineExcelEntity> entities = result.getList();
             for(LineExcelEntity lee:entities){
                 System.out.println(ReflectionToStringBuilder.toString(lee));
